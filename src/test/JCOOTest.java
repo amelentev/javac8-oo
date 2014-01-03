7cf0c3f356e31a1ca034a3c9ef6207e5a8de5b54
Artem Melentyev <amelentev@gmail.com>
2014-01-03 09:04:38 +0300

    idea project files. tests

---
+++ src/test/JCOOTest.java
@@ -0,0 +1,27 @@
+import com.sun.tools.javac.Main;
+
+import java.io.File;
+
+public class JCOOTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println(new File(".").getAbsolutePath());
+        //compile("ValueOf");
+        compile("Math");
+        compile("Cmp");
+        compile("ListIndexGet");
+        compile("ListIndexSet");
+        compile("MapIndex");
+        compile("Vector");
+        compile("CompAss", "../tests");
+    }
+    public static void compile(String clas) throws  Exception {
+        compile(clas, "../examples");
+    }
+    public static void compile(String clas, String path) throws Exception {
+    	String file = path+"/"+clas+".java";
+		System.out.print("Compiling " + file + ": ");
+		boolean res = Main.compile(new String[]{file, "-d", "build/classes/"})==0;
+		res &= (boolean)Class.forName(clas).getDeclaredMethod("test").invoke(null);
+		System.out.println(res ? "ok" : "fail");
+	}
+}
